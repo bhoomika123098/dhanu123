6b7c631e2e2743941b3fbb1008322ad46797a48d
dhanu <dhanu123@gmail.com>
2024-07-11 20:47:45 -0700

    change3

--- simple.java
+++ simple.java
@@ -3,7 +3,7 @@ public class simple java
 	public static void main(String [] args)
 	{
 	   System.out.println("hello tumkur");
-	   System.out.println("gubbi");
+	   System.out.println("gubbi,mysore");
 	}
 }
 
